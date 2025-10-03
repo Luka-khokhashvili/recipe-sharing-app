6aeaf49fcdeec3ee5e873a62c8b54a1a873194da
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-10-03 20:14:07 +0400

    feat: implement add-recipe page with basic form fields, add related route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { RecipeDetailsComponent } from './pages/recipe-details/recipe-details.component';
+import { AddRecipeComponent } from './pages/add-recipe/add-recipe.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'recipe/:id', component: RecipeDetailsComponent },
-  { path: 'add', component: NavbarComponent },
+  { path: 'add', component: AddRecipeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/pages/add-recipe/add-recipe.component.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { Textarea } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { FileUploadModule } from 'primeng/fileupload';
+import { DividerModule } from 'primeng/divider';
+
+@Component({
+  selector: 'app-add-recipe',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    Textarea,
+    ButtonModule,
+    TagModule,
+    FileUploadModule,
+    DividerModule,
+  ],
+  templateUrl: './add-recipe.component.html',
+})
+export class AddRecipeComponent {
+  recipeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.recipeForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      ingredients: this.fb.array([this.fb.control('', Validators.required)]),
+      instructions: this.fb.array([this.fb.control('', Validators.required)]),
+      image: [null, Validators.required],
+    });
+  }
+
+  get ingredients() {
+    return this.recipeForm.get('ingredients') as FormArray;
+  }
+
+  get instructions() {
+    return this.recipeForm.get('instructions') as FormArray;
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.fb.control('', Validators.required));
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+
+  addInstruction() {
+    this.instructions.push(this.fb.control('', Validators.required));
+  }
+
+  removeInstruction(index: number) {
+    this.instructions.removeAt(index);
+  }
+
+  onImageUpload(event: any) {
+    const file = event.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () =>
+        this.recipeForm.patchValue({ image: reader.result });
+      reader.readAsDataURL(file);
+    }
+  }
+
+  submit() {
+    if (this.recipeForm.valid) {
+      console.log('Recipe submitted:', this.recipeForm.value);
+      // TODO: send to RecipeService (POST to JSON server)
+    } else {
+      this.recipeForm.markAllAsTouched();
+    }
+  }
+}
