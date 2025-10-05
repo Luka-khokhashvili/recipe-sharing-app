ef4671e1740ff49842352f8784489a8ad06e7513
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-10-05 19:17:40 +0400

    feat: implement separate details-view component

---
+++ src/app/components/recipe-details/details-view/details-view.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TagModule } from 'primeng/tag';
+import { RatingModule } from 'primeng/rating';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { CommonModule } from '@angular/common';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { Recipe } from '../../../interfaces/recipe';
+
+@Component({
+  selector: 'app-details-view',
+  imports: [
+    CommonModule,
+    TagModule,
+    RatingModule,
+    ButtonModule,
+    DividerModule,
+    ConfirmDialog,
+  ],
+  templateUrl: './details-view.component.html',
+  styleUrl: './details-view.component.css',
+})
+export class DetailsViewComponent {
+  @Input() editMode!: boolean;
+  @Input() recipe!: Recipe | undefined;
+  @Output() favoriteToggled = new EventEmitter<void>();
+  @Output() editRequested = new EventEmitter<void>();
+  @Output() deleteRequested = new EventEmitter<PointerEvent>();
+
+  onFavorite() {
+    this.favoriteToggled.emit();
+  }
+  onToggleEdit() {
+    this.editRequested.emit();
+  }
+  onDelete($event: PointerEvent) {
+    this.deleteRequested.emit($event);
+  }
+}
